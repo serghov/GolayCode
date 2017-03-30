520adfc963308419b01fb34902175eeacb6ab1f7
serghov <sergey.hovakimyan@gmail.com>
2017-03-30 20:31:30 +0400

    Fixed the issue in step 6

--- golayCode.js
+++ golayCode.js
@@ -1,101 +1,101 @@
 class GolayCode {
-	constructor() {
-		throw new Error('Golay code is not an instantiatable class, use static methods instead');
-	}
+    constructor() {
+        throw new Error('Golay code is not an instantiatable class, use static methods instead');
+    }
 
-	static encode(vec) {
-		if (!vec.isBinaryMatrix) {
-			vec = new BinaryMatrix(1, 12, vec);
-		}
-		const mRes = vec.mult(GolayCode.G);
-		return mRes.getRow(0);
-	}
+    static encode(vec) {
+        if (!vec.isBinaryMatrix) {
+            vec = new BinaryMatrix(1, 12, vec);
+        }
+        const mRes = vec.mult(GolayCode.G);
+        return mRes.getRow(0);
+    }
 
-	static decode(vec) {
-		let u = null;
+    static decode(vec) {
+        let u = null;
 
-		if (!vec.isBinaryMatrix) {
-			vec = new BinaryMatrix(1, 24, vec);
-		}
+        if (!vec.isBinaryMatrix) {
+            vec = new BinaryMatrix(1, 24, vec);
+        }
 
-		// step 1
-		GolayCode.Log('Step 1');
-		const syndrome = vec.mult(GolayCode.H.transpose()).getRow(0);
-		GolayCode.Log('Calculated Syndrome', syndrome.join(', '));
+        // step 1
+        GolayCode.Log('Step 1');
+        const syndrome = vec.mult(GolayCode.H.transpose()).getRow(0);
+        GolayCode.Log('Calculated Syndrome', syndrome.join(', '));
 
-		vec = vec.getRow(0);
+        vec = vec.getRow(0);
 
-		// step 2
-		const syndromeWeight = weight(syndrome);
-		GolayCode.Log('Step 2');
-		GolayCode.Log('Syndrome weight is', syndromeWeight);
+        // step 2
+        const syndromeWeight = weight(syndrome);
+        GolayCode.Log('Step 2');
+        GolayCode.Log('Syndrome weight is', syndromeWeight);
 
-		if (syndromeWeight <= 3) {
-			GolayCode.Log('Syndrome weight is less than or equal to 3, solution found!');
-			u = syndrome.concat(makeArray(12));
-			return modTwo(addVectors(u, vec));
-			//return u;
-		}
+        if (syndromeWeight <= 3) {
+            GolayCode.Log('Syndrome weight is less than or equal to 3, solution found!');
+            u = syndrome.concat(makeArray(12));
+            return modTwo(addVectors(u, vec));
+            //return u;
+        }
 
-		// step 3
-		GolayCode.Log('Step 3');
+        // step 3
+        GolayCode.Log('Step 3');
 
-		const bRows = GolayCode.B.getRows();
-		for (let i in bRows) {
-			const cur = modTwo(addVectors(syndrome, bRows[i]));
-			if (weight(cur) <= 2) {
-				GolayCode.Log(`B_${i} is a good match, the weight of S+B_${i} is ${weight(cur)} <= 2`);
-				u = cur.concat(eyeVector(12, i));
-				return modTwo(addVectors(u, vec));
-				//return u;
-			}
-		}
+        const bRows = GolayCode.B.getRows();
+        for (let i in bRows) {
+            const cur = modTwo(addVectors(syndrome, bRows[i]));
+            if (weight(cur) <= 2) {
+                GolayCode.Log(`B_${i} is a good match, the weight of S+B_${i} is ${weight(cur)} <= 2`);
+                u = cur.concat(eyeVector(12, i));
+                return modTwo(addVectors(u, vec));
+                //return u;
+            }
+        }
 
-		// step 4
-		GolayCode.Log('Step 4');
+        // step 4
+        GolayCode.Log('Step 4');
 
-		const secondSyndrome = new BinaryMatrix(1, syndrome.length, syndrome).mult(GolayCode.B).getRow(0);
-		GolayCode.Log('Calculated second syndrome, it is ', secondSyndrome);
+        const secondSyndrome = new BinaryMatrix(1, syndrome.length, syndrome).mult(GolayCode.B).getRow(0);
+        GolayCode.Log('Calculated second syndrome, it is ', secondSyndrome);
 
-		// step 5
-		GolayCode.Log('Step 5');
+        // step 5
+        GolayCode.Log('Step 5');
 
-		if (weight(secondSyndrome) <= 3) {
-			GolayCode.Log(`Second syndrome weight is ${weight(secondSyndrome)} <= 3`);
-			u = makeArray(12).concat(secondSyndrome);
-			return modTwo(addVectors(u, vec));
-			//return u;
-		}
+        if (weight(secondSyndrome) <= 3) {
+            GolayCode.Log(`Second syndrome weight is ${weight(secondSyndrome)} <= 3`);
+            u = makeArray(12).concat(secondSyndrome);
+            return modTwo(addVectors(u, vec));
+            //return u;
+        }
 
-		// step 6
-		GolayCode.Log('Step 6');
+        // step 6
+        GolayCode.Log('Step 6');
 
-		for (let i in bRows) {
-			const cur = modTwo(addVectors(secondSyndrome, bRows[i]));
-			if (weight(cur) <= 2) {
-				GolayCode.Log(`B_${i} is a good match, the weight of SS+B_${i} is ${weight(cur)} <= 2`);
-				u = cur.concat(eyeVector(12, i));
-				return modTwo(addVectors(u, vec));
-				//return u;
-			}
-		}
+        for (let i in bRows) {
+            const cur = modTwo(addVectors(secondSyndrome, bRows[i]));
+            if (weight(cur) <= 2) {
+                GolayCode.Log(`B_${i} is a good match, the weight of SS+B_${i} is ${weight(cur)} <= 2`);
+                u = eyeVector(12, i).concat(cur);
+                return modTwo(addVectors(u, vec));
+                //return u;
+            }
+        }
 
-		// step 7
-		GolayCode.Log('Step 7');
-		GolayCode.Log('Cant find solution, data is too corrupted');
-		return [];
+        // step 7
+        GolayCode.Log('Step 7');
+        GolayCode.Log('Cant find solution, data is too corrupted');
+        return [];
 
-	}
+    }
 
-	static Log(...args) {
-		for (let i in GolayCode.logHandlers) {
-			GolayCode.logHandlers[i](...args);
-		}
-	}
+    static Log(...args) {
+        for (let i in GolayCode.logHandlers) {
+            GolayCode.logHandlers[i](...args);
+        }
+    }
 
-	static addLogHandler(fcn) {
-		GolayCode.logHandlers.push(fcn);
-	}
+    static addLogHandler(fcn) {
+        GolayCode.logHandlers.push(fcn);
+    }
 
 }
 
